69468bf78af8d7eeea7bf6433bf7ed67e9c50aee
Million-art <74927604+Million-art@users.noreply.github.com>
2023-01-09 19:22:27 +0300

    EmployeeManagement

---
+++ EmployeesManagement/EmployeesManagement/Context/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿ using Microsoft.EntityFrameworkCore;
+using EmployeesManagement.Models;
+namespace EmployeesManagement.Context
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> contextOptions)
+            :base(contextOptions)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeesManagement/EmployeesManagement/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿ 
+namespace EmployeesManagement.Models
+{
+    public class Employee
+    {
+         public int Id { get; set; }
+         public String Name { get; set; }
+        public String Lname { get; set; }
+         public String Email { get; set; }
+        public  int Phone { get; set; }
+    }
+}
